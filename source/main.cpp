a19d308f89892a0b4e57b83fb2d0315d441ae821
Luiz Miguel <contato.luiz404@gmail.com>
2026-08-29 15:12:03 -0300

    #010: Atualização do main.cpp (teste)

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,22 @@
 #include <nds.h>
 #include <stdio.h>
 
-volatile int frame = 0;
-
-void Vblank() {
-    frame++;
-}
+#include "font_bin.h"
 
 int main(void) {
-    consoleDemoInit();
-    irqSet(IRQ_VBLANK, Vblank);
-    iprintf("Hello, world!\n");
-    iprintf("Meu Caderno Portatil");
+    PrintConsole topScreen;
+    consoleInit(&topScreen, 0, BgType_Text4bpp, BgSize_T_256x256,
+                31, 0, true, true);
+
+    consoleSetFont(&topScreen, (ConsoleFont*)font_bin);
+
+    consoleSelect(&topScreen);
+
+    iprintf("\x1b[1;1HHello, world!");
+    iprintf("\x1b[2;1HMeu Caderno Portatil");
+    iprintf("\x1b[3;1HCom uma fonte personalizada!");
 
     while(1) {
-        iprintf("\x1b[10;0HFrame = %d", frame);
         swiWaitForVBlank();
     }
 
