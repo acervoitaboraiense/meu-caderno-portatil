36a5d96d24f4648f155e417285f71bbb4f7330b1
Luiz Miguel <contato.luiz404@gmail.com>
2026-08-29 15:16:22 -0300

    #012: Atualização do main.cpp (teste)

--- source/main.cpp
+++ source/main.cpp
@@ -2,14 +2,20 @@
 #include <stdio.h>
 
 int main(void) {
+    consoleDemoInit();
+
     PrintConsole topScreen;
     consoleInit(&topScreen, 0, BgType_Text4bpp, BgSize_T_256x256,
                 31, 0, true, true);
-    consoleSelect(&topScreen);
 
-    iprintf("\x1b[1;1HHello, world!");
+    iprintf("\x1b[1;1HTela de baixo");
+    iprintf("\x1b[2;1HHello, world!");
+
+    consoleSelect(&topScreen);
+    iprintf("\x1b[1;1HTela de cima");
     iprintf("\x1b[2;1HMeu Caderno Portatil");
     iprintf("\x1b[3;1HPressione START para sair");
+
 
     while(1) {
         swiWaitForVBlank();
