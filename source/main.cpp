b93e25a3fd98ac18a36ba0ad3ba11a496b958fb1
Luiz Miguel <contato.luiz404@gmail.com>
2026-08-29 15:13:37 -0300

    #011: Atualização do main.cpp (teste)

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,19 @@
 #include <nds.h>
 #include <stdio.h>
 
-#include "font_bin.h"
-
 int main(void) {
     PrintConsole topScreen;
     consoleInit(&topScreen, 0, BgType_Text4bpp, BgSize_T_256x256,
                 31, 0, true, true);
-
-    consoleSetFont(&topScreen, (ConsoleFont*)font_bin);
-
     consoleSelect(&topScreen);
 
     iprintf("\x1b[1;1HHello, world!");
     iprintf("\x1b[2;1HMeu Caderno Portatil");
-    iprintf("\x1b[3;1HCom uma fonte personalizada!");
+    iprintf("\x1b[3;1HPressione START para sair");
 
     while(1) {
         swiWaitForVBlank();
+        if (keysDown() & KEY_START) break;
     }
 
     return 0;
